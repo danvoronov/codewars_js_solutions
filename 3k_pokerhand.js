1c8f0fa8fc807cc208f22d6a47a68c4a6062f778
dan.voronov <dan.voronov@gmail.com>
2022-02-17 12:47:51 +0200

    fix poker with prior

--- 3k_pokerhand.js
+++ 3k_pokerhand.js
@@ -24,55 +24,57 @@ function hand(holeCards, communityCards) { // MAIN
   
   let cards = {'♦':[],'♣':[],'♥':[],'♠':[]},  
       allCards=[],
-      repetitions = Array(15).fill(0)
+      amount = Array(15).fill(0)
   
   holeCards.concat(communityCards).forEach(c=>{
     let c2num = crdValue(c.slice(0,-1))
     cards[c.slice(-1)].push(c2num)
     allCards.push(c2num)
-    repetitions[c2num] += 1
+    amount[c2num] += 1
   })
   
-  const [Frep, Srep] = rsort(repetitions),
+  const [Frep, Srep] = rsort(amount),
+  am = amount,  // just shoter name shallow link
   allRCards = rsort([...new Set(allCards)]), // remove dub
     
   five_suit = Object.keys(cards).filter(s=>cards[s].length>=5)[0]
   if (five_suit) {
-    let rank_five = rsort(cards[five_suit]), startSt = isStraight(rank_five)
-    return (startSt==-1) ? hand_obj("flush", _(rank_five,0,5)) // 4
-           : hand_obj("straight-flush", _(rank_five, startSt,startSt+5) ); // 1
+    var rank_five = rsort(cards[five_suit]), Sindex = isStraight(rank_five)
+    if (Sindex>-1) // 1
+      return hand_obj("straight-flush", _(rank_five, Sindex,Sindex+5) )
   }
   
-  let allS = isStraight(allRCards)
-  if (allS > -1) return hand_obj("straight", _(allRCards, allS,allS+5) ); // 5
-  
-  const rep = repetitions // just shoter name
-  
-  if (Frep==4) {
-    let tBr = rep.indexOf(4)
-    return hand_obj("four-of-a-kind", [crdName(tBr), ..._(allRCards.filter(f=>f!=tBr),0,1)] ); // 2
+  if (Frep==4) { //2
+    let tBr = am.lastIndexOf(4) // lastI because greater in the end
+    return hand_obj("four-of-a-kind", [crdName(tBr), ..._(allRCards.filter(f=>f!=tBr),0,1)] ); 
   }                                       
   
-  if (Frep==3 && Srep==2) 
-    return hand_obj("full house", [crdName(rep.indexOf(3)), crdName(rep.indexOf(2))] ); // 3
+  if (Frep==3 && Srep==2)  // 3
+    return hand_obj("full house", [crdName(am.lastIndexOf(3)), crdName(am.lastIndexOf(2))] ); 
+
+  if (five_suit && Sindex==-1) // 4
+    return hand_obj("flush", _(rank_five,0,5))
   
-  if (Frep==3) {
-    let tBr = rep.indexOf(3)
-    return hand_obj("three-of-a-kind", [crdName(tBr), ..._(allRCards.filter(f=>f!=tBr),0,2)] ); // 6
+  let allS = isStraight(allRCards) //5
+  if (allS > -1) return hand_obj("straight", _(allRCards, allS,allS+5) );
+  
+  if (Frep==3) { // 6
+    let tBr = am.lastIndexOf(3)
+    return hand_obj("three-of-a-kind", [crdName(tBr), ..._(allRCards.filter(f=>f!=tBr),0,2)] ); 
   } 
   
-  if (Frep==2 && Srep==2) {
-    let tBr1 = rep.lastIndexOf(2),
-      tBr2 = rep.lastIndexOf(2, tBr1-1)
+  if (Frep==2 && Srep==2) { // 7
+    let tBr1 = am.lastIndexOf(2),
+      tBr2 = am.lastIndexOf(2, tBr1-1)
     return hand_obj("two pair", [crdName(tBr1), crdName(tBr2),
-                                 ..._(allRCards.filter(f=>f!=tBr1 && f!=tBr2),0,1)] ); // 7
+                                 ..._(allRCards.filter(f=>f!=tBr1 && f!=tBr2),0,1)] ); 
   } 
 
-  if (Frep==2) {
-    let tBr = rep.indexOf(2)
-    return hand_obj("pair", [crdName(tBr), ..._(allRCards.filter(f=>f!=tBr),0,3)] ); // 8
+  if (Frep==2) { // 8
+    let tBr = am.lastIndexOf(2)
+    return hand_obj("pair", [crdName(tBr), ..._(allRCards.filter(f=>f!=tBr),0,3)] ); 
   } 
   
-  return  hand_obj("nothing", _(allRCards,0,5));
+  return hand_obj("nothing", _(allRCards,0,5)); // 9
 
 }
